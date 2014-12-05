d3b09eb1d634eee6038abfba6fe0b200118df7a1
Shawn Johnson <johnson_shawn@bah.com>
2014-12-05 12:25:02 -0500

    a lot of changes

--- index.md
+++ index.md
@@ -13,7 +13,6 @@ We built the API to be as self-documenting as possible, but if you find yourself
 
 - [API basics](basics.html) introduces you to the operations offered by the API.
 - [API calls](console/) gives you a hands-on experience of those operations with an interactive console.
-- [Query language](queries.html) contains a comprehensive listing of all the API's types, functions, and enumerations.
 - [Field reference](fields.html) lists and describes the type of information returned by the API.
 
 <body id="overview"></body>
--- static/js/script.js
+++ static/js/script.js
@@ -12,41 +12,10 @@ $(function() {
       }
     }
   });
+  
+  addCodeTable('static/document-fields.json','document-fields');
+  addCodeTable('static/docket-fields.json','docket-fields');
 
-  $("#document-fields tbody").each(function () {
-    var $body = $(this);
-
-    var ajax = $.ajax({
-      url: "static/document-fields.json",
-      dataType: "json"
-    });
-
-    ajax.done(function (data) {
-      var fields = data['fields'];
-      var fieldNames = _(fields).keys().sort();
-
-      _(fieldNames).each(function (fieldName) {
-        var info = fields[fieldName];
-        var example = "";
-        if(info['could_be'] != null) {
-        	example = "<p><p><em>Ex. </em><code>" + info['could_be'] + "</code>";
-        }
-		var description = "<td>" + info['description'] + example;
-		if (info['url'] != null) {
-			description += "<p><a href='" + info['url'] + "'>More Info</a>";
-		}
-		description += "</td>";
-        var $row = $("<tr></tr>");
-        $row.
-          append("<td><code>" + fieldName + "</code></td>").
-					append(description).
-					append("<td>" + info['type'] + "</td>");
-					//append("<td>" + info['could_be'] + "</td>");
-          //append("<td>" + (info['indexed'] ? "true" : "false") + "</td>");
-        $body.append($row);
-      });
-    });
-  });
 });
 
 // jsonUrl: URL to the JSON-formatted metadata
@@ -55,64 +24,36 @@ $(function() {
 //   k maps to the key to pull from the JSON
 //   h is the text for the header at the top of the table
 function addCodeTable(jsonUrl, tableId, orderAndDesc) {
+	var $tbody = $('#' + tableId + ' tbody');
+	
 	var ajax = $.ajax({
 		url: jsonUrl,
 		dataType: "json"
 	});
 	var data = null;
-	ajax.done(function(data) {
-		// don't worry about processing anything not an array
-		console.log('Got the JSON');
-		console.log(data);
-		if (!$.isArray(data) || data.length == 0) {
-			console.log(' Bad array');
-			return;
-		}
-		// populate the orderAndDesc array
-		if (!orderAndDesc) {
-			orderAndDesc = json[0].keys().map(function(k) {
-				return {key: k, header: k};
-			});
-		}
-		console.log('Set Order and Desc');
-		console.log(orderAndDesc);
-		$('#' + tableId).each(function() {
-			var $body = $(this);
-			if ($body.length) {
-				console.log('Found table #' + tableId);
-			}
-			// The expectation is that the data is in an array 
-			// of objects with the same fields in them
-			// the orderAndDesc array is an array of keys mapping to a description
-			// of the key used at the header of the table.
+    ajax.done(function (data) {
+        var fields = data['fields'];
+        var fieldNames = _(fields).keys().sort();
 
-			// Add the header
-
-			var $thead = $body.find('thead');
-			console.log('THEAD ' + $thead);
-			var $theadrow = $('<tr></tr>');
-			_(orderAndDesc).each(function(head) {
-				$theadrow.append('<th>' + head.header + '</th>');
-			});
-			$thead.append($theadrow);
-			$body = $body.append($thead);
-			// Add the data
-			$tbody = $body.find('tbody');
-			_(data).each(function(row) {
-				var $tr = $('<tr></tr>');
-				_(orderAndDesc).each(function(k) {
-					$tr.append('<td>' + row[k.key] + '</td>');
-				});
-				$tbody.append($tr);
-			});
-			$body.append($tbody);
-		});
-	});
-	console.log('Testing for data != null');
-
-	if (data == null) {
-		return;
-	}
-	console.log('Got ' + data.length + ' codes');
-
+        _(fieldNames).each(function (fieldName) {
+          var info = fields[fieldName];
+          var example = "";
+          if(info['could_be'] != null) {
+          	example = "<p><p><em>Ex. </em><code>" + info['could_be'] + "</code>";
+          }
+  		var description = "<td>" + info['description'] + example;
+  		if (info['url'] != null) {
+  			description += "<p><a href='" + info['url'] + "'>More Info</a>";
+  		}
+  		description += "</td>";
+          var $row = $("<tr></tr>");
+          $row.
+            append("<td><code>" + fieldName + "</code></td>").
+  					append(description).
+  					append("<td>" + info['type'] + "</td>");
+  					//append("<td>" + info['could_be'] + "</td>");
+            //append("<td>" + (info['indexed'] ? "true" : "false") + "</td>");
+          $tbody.append($row);
+        });
+    });
 }
