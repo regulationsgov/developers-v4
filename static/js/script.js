c7c36a17858f97739b523cf565d8c1b19fe22744
Shawn Johnson <johnson_shawn@bah.com>
2014-12-08 16:36:56 -0500

    Renamed method, removed data/page-specific script.

--- static/js/script.js
+++ static/js/script.js
@@ -12,18 +12,15 @@ $(function() {
       }
     }
   });
-  
-  addCodeTable('static/document-fields.json','document-fields');
-  addCodeTable('static/docket-fields.json','docket-fields');
-
 });
 
+// Method to add field descriptions to the fields.html
 // jsonUrl: URL to the JSON-formatted metadata
 // tableId: The table to populate
 // orderAndDesc: An array of objects with keys { key: k, header: h }
 //   k maps to the key to pull from the JSON
 //   h is the text for the header at the top of the table
-function addCodeTable(jsonUrl, tableId, orderAndDesc) {
+function addFieldTable(jsonUrl, tableId, orderAndDesc) {
 	var $tbody = $('#' + tableId + ' tbody');
 	
 	var ajax = $.ajax({
@@ -56,4 +53,73 @@ function addCodeTable(jsonUrl, tableId, orderAndDesc) {
           $tbody.append($row);
         });
     });
+}
+
+// Method to add code descriptions to the codes.html
+// jsonUrl: URL to the JSON-formatted metadata
+// tableId: The table to populate
+// orderAndDesc: An array of objects with keys { key: k, header: h }
+//   k maps to the key to pull from the JSON
+//   h is the text for the header at the top of the table
+function addCodeTable(jsonUrl, tableId, orderAndDesc) {
+	var ajax = $.ajax({
+		url: jsonUrl,
+		dataType: "json"
+	});
+	var data = null;
+	ajax.done(function(data) {
+		// don't worry about processing anything not an array
+		console.log('Got the JSON');
+		console.log(data);
+		if (!$.isArray(data) || data.length == 0) {
+			console.log(' Bad array');
+			return;
+		}
+		// populate the orderAndDesc array
+		if (!orderAndDesc) {
+			orderAndDesc = json[0].keys().map(function(k) {
+				return {key: k, header: k};
+			});
+		}
+		console.log('Set Order and Desc');
+		console.log(orderAndDesc);
+		$('#' + tableId).each(function() {
+			var $body = $(this);
+			if ($body.length) {
+				console.log('Found table #' + tableId);
+			}
+			// The expectation is that the data is in an array 
+			// of objects with the same fields in them
+			// the orderAndDesc array is an array of keys mapping to a description
+			// of the key used at the header of the table.
+
+			// Add the header
+
+			var $thead = $body.find('thead');
+			console.log('THEAD ' + $thead);
+			var $theadrow = $('<tr></tr>');
+			_(orderAndDesc).each(function(head) {
+				$theadrow.append('<th>' + head.header + '</th>');
+			});
+			$thead.append($theadrow);
+			$body = $body.append($thead);
+			// Add the data
+			$tbody = $body.find('tbody');
+			_(data).each(function(row) {
+				var $tr = $('<tr></tr>');
+				_(orderAndDesc).each(function(k) {
+					$tr.append('<td>' + row[k.key] + '</td>');
+				});
+				$tbody.append($tr);
+			});
+			$body.append($tbody);
+		});
+	});
+	console.log('Testing for data != null');
+
+	if (data == null) {
+		return;
+	}
+	console.log('Got ' + data.length + ' codes');
+
 }
